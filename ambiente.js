ea7023eacb5ea89744a480f0b76b7efd054a3169
Gabriel <gabrielmartinellids@gmail.com>
2022-03-22 17:43:46 -0300

    commit

---
+++ ambiente.js
@@ -0,0 +1,22 @@
+var c = 1
+do {
+    console.log(`passo $ { c }`)
+    c++
+} while (c <= 10)
+
+
+
+
+
+
+
+
+
+
+
+/*console.log("tudo bem")
+console.log("tudo bem")
+console.log("tudo bem")
+console.log("tudo bem")
+console.log("tudo bem")
+console.log("tudo bem") */
